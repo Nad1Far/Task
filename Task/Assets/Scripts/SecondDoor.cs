660bb4db94107bdb6f5d2bbc5b07390fd04b76a4
Nad1Far <147243971+Nad1Far@users.noreply.github.com>
2024-05-25 14:16:58 +0500

    незначительные изменения

--- Task/Assets/Scripts/SecondDoor.cs
+++ Task/Assets/Scripts/SecondDoor.cs
@@ -20,24 +20,25 @@ public class SecondDoor : MonoBehaviour
 
     void Update()
     {
-            if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (Vector3.Distance(Player.transform.position, Door.transform.position) <= Distance && Player.GetComponent<Arms>().RightArmClear == false && FirstKey.GetComponent<KeyScript>().KeyIs == true)
             {
-                if (Vector3.Distance(Player.transform.position, Door.transform.position) <= Distance && Player.GetComponent<Arms>().RightArmClear == false && FirstKey.GetComponent<KeyScript>().KeyIs == true)
-                {
-                    FirstKey.SetActive(false);
-                    Player.GetComponent<Arms>().RightArmClear = true;
-                    FirstKeyIsCollected = true;
-                    SignE.SetActive(false);
-                    StartCoroutine(RotateObjectSmoothly());
-                    SignE.SetActive(false);
-                    Door.GetComponent<BoxCollider>().enabled = false;
-                }
+                FirstKey.SetActive(false);
+                Player.GetComponent<Arms>().RightArmClear = true;
+                FirstKeyIsCollected = true;
+                SignE.SetActive(false);
+                StartCoroutine(RotateObjectSmoothly());
+                SignE.SetActive(false);
+                Door.GetComponent<BoxCollider>().enabled = false;
             }
+        }
 
 
         if (Vector3.Distance(Player.transform.position, Door.transform.position) <= Distance && FirstKeyIsCollected == false)
         {
             SignE.SetActive(true);
+            SignE.GetComponent<Renderer>().material.color = Color.red;
 
         }
         else
@@ -45,24 +46,28 @@ public class SecondDoor : MonoBehaviour
             SignE.SetActive(false);
         }
 
-    }
-
-
-
-    IEnumerator RotateObjectSmoothly()
-    {
-        Quaternion startRotation = DoorInDoor.transform.rotation;
-        Quaternion targetRotation = Quaternion.Euler(DoorInDoor.transform.rotation.x, targetYAngle, DoorInDoor.transform.rotation.z);
-
-        float elapsedTime = 0f;
-
-        while (elapsedTime < rotateTime)
+        if (Vector3.Distance(Player.transform.position, Door.transform.position) <= Distance && Player.GetComponent<Arms>().RightArmClear == false && FirstKey.GetComponent<KeyScript>().KeyIs == true)
         {
-            DoorInDoor.transform.rotation = Quaternion.Lerp(startRotation, targetRotation, elapsedTime / rotateTime);
-            elapsedTime += Time.deltaTime;
-            yield return null;
+            SignE.GetComponent<Renderer>().material.color = Color.white;
         }
 
-        DoorInDoor.transform.rotation = targetRotation;
+
+
+        IEnumerator RotateObjectSmoothly()
+        {
+            Quaternion startRotation = DoorInDoor.transform.rotation;
+            Quaternion targetRotation = Quaternion.Euler(DoorInDoor.transform.rotation.x, targetYAngle, DoorInDoor.transform.rotation.z);
+
+            float elapsedTime = 0f;
+
+            while (elapsedTime < rotateTime)
+            {
+                DoorInDoor.transform.rotation = Quaternion.Lerp(startRotation, targetRotation, elapsedTime / rotateTime);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            DoorInDoor.transform.rotation = targetRotation;
+        }
     }
 }
