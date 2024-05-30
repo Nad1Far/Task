4178ff5d1da7e97ca9a6fb2561aa4abf140aea4d
Nad1Far <147243971+Nad1Far@users.noreply.github.com>
2024-05-30 22:42:44 +0500

    StartUI

--- Task/Assets/Scripts/PlayerHealth.cs
+++ Task/Assets/Scripts/PlayerHealth.cs
@@ -7,9 +7,19 @@ public class PlayerHealth : MonoBehaviour
 {
     public AudioSource BackgroundMusic;
     public AudioSource DeathSound;
+    public GameObject StartUI;
     public GameObject GameOverUI;
 
     public bool PlayerIsDead = false;
+
+
+    private void Start()
+    {
+        StartUI.SetActive(true);
+        GetComponent<PlayerController>().enabled = false;
+        GetComponent<CameraController>().enabled = false;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Bullet" || other.gameObject.tag == "Laser" || other.gameObject.tag == "Shape")
@@ -34,6 +44,14 @@ public class PlayerHealth : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape) && GameOverUI.activeSelf == true)
         {
             SceneManager.LoadScene("Menu");
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && StartUI.activeSelf == true)
+        {
+            StartUI.SetActive(false);
+            GetComponent<PlayerController>().enabled = true;
+            GetComponent<CameraController>().enabled = true;
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && GameOverUI.activeSelf == true)
